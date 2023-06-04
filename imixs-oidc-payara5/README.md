969ed2516a6d18407cb685f12b14f9bb2e5ea830
Ralph Soika <ralph.soika@imixs.com>
2023-06-04 13:29:15 +0200

    refactoring
    Issue #2

--- imixs-oidc-payara5/README.md
+++ imixs-oidc-payara5/README.md
@@ -2,21 +2,17 @@
 
 This project provides a generic library to setup an OpenID Connect security mechanism for web applications running in Payara 5. Payara 5 provide a custom implementation to support OpenID Connect. You can find details in [Gaurav Gupta Blogpost](https://blog.payara.fish/openid-connect-in-the-payara-platform-5.183) as also in the [Blogpost from Rudy De Busscher](https://blog.payara.fish/how-to-use-openidconnect-with-payara-platform-to-achieve-single-sign-on). The implementation was inspired by [Andrew Hughes blogpost about Jakarta EE and OIDC](https://auth0.com/blog/jakarta-ee-oidc/).
 
-**Note:** The `@OpenIdAuthenticationDefinition` used in this library is a proprietary extension from Payara 5. The implementation was later adapted into the [Jakarata Security API 3.0](https://jakarta.ee/specifications/security/3.0/) which is now part of Jakarta 10. We provide this library for projects still running on Payara 5. 
-
+**Note:** The `@OpenIdAuthenticationDefinition` used in this library is a proprietary extension from Payara 5. The implementation was later adapted into the [Jakarata Security API 3.0](https://jakarta.ee/specifications/security/3.0/) which is now part of Jakarta 10. We provide this library for projects still running on Payara 5.
 
 ## Background
 
-Since Jakarta EE 8 a new Security API was introduced providing a new standard and a portable way of handling security concerns in Java containers. This new standard allows to configure the Authentication mechanism of an application directly in a CDI bean instead through the web.xml file. So web applications can now configure Authentication mechanisms by providing implementations of the new `HttpAuthenticationMechanism` interface. Beside the standard implementations for Basic, Form and CustomForm authentication Payara 5 adapted very early this concept to provide authentication mechanism for OpenID Connect. To avoid to pollute the application code with proprietary code we introduced this library which can be added simply as an dependency to a web application project. 
+Since Jakarta EE 8 a new Security API was introduced providing a new standard and a portable way of handling security concerns in Java containers. This new standard allows to configure the Authentication mechanism of an application directly in a CDI bean instead through the web.xml file. So web applications can now configure Authentication mechanisms by providing implementations of the new `HttpAuthenticationMechanism` interface. Beside the standard implementations for Basic, Form and CustomForm authentication Payara 5 adapted very early this concept to provide authentication mechanism for OpenID Connect. To avoid to pollute the application code with proprietary code we introduced this library which can be added simply as an dependency to a web application project.
 
-**Note:** Also this solution is not perfect as it requires to create a custom build of your application code including this library. But it avoids the need to add implement the `fish.payara.security.annotations.OpenIdAuthenticationDefinition` directly in your application code. 
-
-
+**Note:** Also this solution is not perfect as it requires to create a custom build of your application code including this library. But it avoids the need to add implement the `fish.payara.security.annotations.OpenIdAuthenticationDefinition` directly in your application code.
 
 ## Maven Dependecy
 
 To use this lib you need to add the following maven dependencies to your application:
-
 
 ```xml
     <!-- Payara 5 OpenID Connect-->
@@ -27,11 +23,11 @@ To use this lib you need to add the following maven dependencies to your applica
     </dependency>
 ```
 
-Payara 5 automatically scann this library during deployment and initializes the OpenID Connect auth mechanism for your application. This is possible because of the `beans.xml` file located in the META-INF folder of this library. 
+Payara 5 automatically scann this library during deployment and initializes the OpenID Connect auth mechanism for your application. This is possible because of the `beans.xml` file located in the META-INF folder of this library.
 
 ## Auth0 OIDC
 
-For testing and also production you can easily setup an OpenID login account with [auth0](https://auth0.com/). 
+For testing and also production you can easily setup an OpenID login account with [auth0](https://auth0.com/).
 If you have not already, [install the Auth0 CLI](https://github.com/auth0/auth0-cli#installation) and run auth0 login in a terminal:
 
 ```
@@ -55,13 +51,13 @@ If you already have setup a tenant you can check the existing domain with
 
 ```
 $ auth0 tenants list
-===  
+===
 
-  AVAILABLE TENANTS    
-  foo.eu.auth0.com 
+  AVAILABLE TENANTS
+  foo.eu.auth0.com
 ```
 
-The tenant domain is your Auth0 domain and needed for further configuration. 
+The tenant domain is your Auth0 domain and needed for further configuration.
 
 Now use the Auth0 CLI to create an OpenID Connect (OIDC) application:
 
@@ -75,11 +71,9 @@ auth0 apps create
     Allowed Logout URLs: http://localhost:8080
 ```
 
-
 The console output shows you the Auth0 domain and the OIDC client ID. However, you also need the client secret. You will find this on the dashboard when your are logged into [Auth0](https://manage.auth0.com)
 
 <img src="doc/images/auth0-001.png">
-
 
 ## Configuration
 
@@ -103,6 +97,10 @@ Setting the properties in Docker or a Kubernetes environment use the correspondi
     PAYARA_SECURITY_OPENID_REDIRECTURI
 ```
 
+## Debug
 
+You can request details about the authenticated user with the Rest API endpoint /debug:
 
+    https://localhost:8181/api/oidc/debug
 
+This information can be helpful to analyze tokens and Claims returned by the OpenID provider.
--- imixs-oidc-payara5/src/main/java/org/imixs/oidc/Securitybean.java
+++ imixs-oidc-payara5/src/main/java/org/imixs/oidc/Securitybean.java
@@ -1,28 +1,23 @@
 package org.imixs.oidc;
 
+import java.io.Serializable;
+import java.security.Principal;
+import java.util.logging.Logger;
 
 import javax.annotation.security.DeclareRoles;
 import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.inject.Named;
-import javax.servlet.annotation.HttpConstraint;
-import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import java.io.PrintWriter;
-import java.security.Principal;
-import java.util.List;
-import java.io.Serializable;
-import javax.naming.NamingException;
+import javax.ws.rs.core.MediaType;
+
 import fish.payara.cdi.auth.roles.RolesPermitted;
 import fish.payara.security.annotations.ClaimsDefinition;
 import fish.payara.security.annotations.OpenIdAuthenticationDefinition;
-import fish.payara.security.openid.api.Claims;
 import fish.payara.security.openid.api.OpenIdContext;
-import java.util.logging.Logger;
-import javax.ws.rs.core.MediaType;
+
 /**
  * 
  * See: https://auth0.com/blog/jakarta-ee-oidc/
@@ -31,96 +26,104 @@ import javax.ws.rs.core.MediaType;
 
 // @Named
 @RequestScoped
-@Path("/hello")
+@Path("/oidc")
 @Produces({ MediaType.TEXT_PLAIN })
 @DeclareRoles({ "org.imixs.ACCESSLEVEL.NOACCESS", "org.imixs.ACCESSLEVEL.READERACCESS",
-"org.imixs.ACCESSLEVEL.AUTHORACCESS", "org.imixs.ACCESSLEVEL.EDITORACCESS",
-"org.imixs.ACCESSLEVEL.MANAGERACCESS" })
-@OpenIdAuthenticationDefinition(
-        providerURI = "${payara.security.openid.providerURI}",
-        clientId = "${payara.security.openid.clientId}",
-        clientSecret = "${payara.security.openid.clientSecret}",
-        redirectURI = "${payara.security.openid.redirectURI}",
-        scope = {"email","openid","profile"}
-        //claimsDefinition = @ClaimsDefinition(
-        //        callerGroupsClaim = "b2xroles",
-        //        callerNameClaim = "employeenumber"
-        //)
+        "org.imixs.ACCESSLEVEL.AUTHORACCESS", "org.imixs.ACCESSLEVEL.EDITORACCESS",
+        "org.imixs.ACCESSLEVEL.MANAGERACCESS" })
+@OpenIdAuthenticationDefinition( //
+        providerURI = "${payara.security.openid.providerURI}", //
+        clientId = "${payara.security.openid.clientId}", //
+        clientSecret = "${payara.security.openid.clientSecret}", //
+        redirectURI = "${payara.security.openid.redirectURI}", //
+        scope = {
+                "email", "openid", "profile" }, //
+        claimsDefinition = @ClaimsDefinition(callerGroupsClaim = "http://www.imixs.org/roles") //
 )
 public class Securitybean implements Serializable {
-   
+
     private static final long serialVersionUID = 1L;
-   private static Logger logger = Logger.getLogger(Securitybean.class.getName());
- 
-  
+    private static Logger logger = Logger.getLogger(Securitybean.class.getName());
 
-      @Inject
-      Principal principal;
+    @Inject
+    Principal principal;
 
-      @Inject
-      private OpenIdContext context;
-  
-      @GET
-      @Path("/principal")
-      @RolesAllowed("org.imixs.ACCESSLEVEL.MANAGERACCESS")
-      @Produces("text/plain")
-      public String getPrincipal() {
-         logger.info("========> getPrincipal");
-          return principal.getName();
-      }
-  
-      @GET
-      @Path("/allowed")
-      @RolesPermitted({"org.imixs.ACCESSLEVEL.MANAGERACCESS"})
-      @Produces("text/plain")
-      public String allowed() {
-         logger.info("========> allowed");
-          return "Allowed";
-      }
-  
-      @GET
-      @Path("/allowedVIP")
-      @RolesAllowed("org.imixs.ACCESSLEVEL.MANAGERACCESS")
-      @Produces("text/plain")
-      public String allowedVIP() {
-         logger.info("========> allowedVIP");
-          return "Allowed for VIPs only!";
-      }
-  
-      @GET
-      @Path("/forbidden")
-      @RolesAllowed("org.imixs.ACCESSLEVEL.MANAGERACCESS")
-      @Produces("text/plain")
-      public String forbidden() {
-         logger.info("========> forbidden");
-          return "Should not be here";
-      }
-  
-      @GET
-      @Produces("text/plain")
-      public String hello() {
-          return "Hello, World!";
-      }
-  
-      @GET
-      @Path("/session-info")
-      @RolesAllowed({"org.imixs.ACCESSLEVEL.MANAGERACCESS"})
-      @Produces("text/plain")
-      public String sessionInfoAuth() {
-          StringBuilder stringBuilder = new StringBuilder();
-          //Here's the caller name
-          stringBuilder.append("Caller name = " + context.getCallerName());
-          //Here's the caller groups
-          stringBuilder.append("Caller groups = " + context.getCallerGroups());
-          //Here's the unique subject identifier within the issuer
-          stringBuilder.append("Subject = " + context.getSubject());
-          //Here's the access token
-          stringBuilder.append("Access token = " + context.getAccessToken());
-          //Here's the identity token
-          stringBuilder.append("ID token = " + context.getIdentityToken());
-          //Here's the user claims
-          stringBuilder.append("Claims json = " + context.getClaimsJson());
-          return stringBuilder.toString();
-      }
+    @Inject
+    private OpenIdContext context;
+
+    @GET
+    @Path("/principal")
+    @RolesAllowed("org.imixs.ACCESSLEVEL.MANAGERACCESS")
+    @Produces("text/plain")
+    public String getPrincipal() {
+        logger.info("========> getPrincipal");
+        return principal.getName();
+    }
+
+    @GET
+    @Path("/allowed")
+    @RolesPermitted({ "org.imixs.ACCESSLEVEL.MANAGERACCESS" })
+    @Produces("text/plain")
+    public String allowed() {
+        logger.info("========> allowed");
+        return "Allowed";
+    }
+
+    @GET
+    @Path("/allowedVIP")
+    @RolesAllowed("org.imixs.ACCESSLEVEL.MANAGERACCESS")
+    @Produces("text/plain")
+    public String allowedVIP() {
+        logger.info("========> allowedVIP");
+        return "Allowed for VIPs only!";
+    }
+
+    @GET
+    @Path("/forbidden")
+    @RolesAllowed("org.imixs.ACCESSLEVEL.MANAGERACCESS")
+    @Produces("text/plain")
+    public String forbidden() {
+        logger.info("========> forbidden");
+        return "Should not be here";
+    }
+
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+    @GET
+    @Path("/debug")
+    // @RolesAllowed({ "org.imixs.ACCESSLEVEL.MANAGERACCESS" })
+    @Produces("text/plain")
+    public String sessionInfoAuth() {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            logger.info("collecting OpenID context information...");
+            // Here's the caller name
+            stringBuilder.append("\nCaller name = " + context.getCallerName());
+            // Here's the caller groups
+            stringBuilder.append("\nCaller groups = " + context.getCallerGroups());
+            // Here's the unique subject identifier within the issuer
+            if (context.getSubject() == null) {
+                stringBuilder.append("\nSubject = undefined");
+            } else {
+                stringBuilder.append("\nSubject = " + context.getSubject());
+            }
+            // Here's the access token
+            stringBuilder.append("\nAccess token = " + context.getAccessToken());
+            // Here's the identity token
+            stringBuilder.append("\nID token = " + context.getIdentityToken());
+            // Here's the user claims
+            stringBuilder.append("\n\nClaims json = " + context.getClaimsJson());
+        } catch (Exception e) {
+
+            stringBuilder.append("\n\nFailed to resolve OpenIdContext: " + e.getMessage());
+        }
+        return stringBuilder.toString();
+    }
 
 }
